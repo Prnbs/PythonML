a45669f7d8f346c111bfa9a5b27f0d3d19f99f72
Pranabesh Sinha <pranabesh.sinha@gmail.com>
2015-08-19 18:34:19 -0700

    some code cleanup

--- YelpReviewAnalyze.py
+++ YelpReviewAnalyze.py
@@ -3,31 +3,56 @@ __author__ = 'prnbs'
 from pymongo import MongoClient
 import nltk
 from nltk.corpus import stopwords
+from timeit import default_timer as timer
+import pandas as pd
+import itertools
 
-sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-bigram = []
 
-client = MongoClient()
-db = client['yelp']
-reviews = db['reviews']
-documents = reviews.find({"stars" : 5},{'text' : 1}).limit(200)
-for doc in documents:
-    text = doc.get('text')
-    # smartish word tokenizer, not too reliable
-    sentences = sent_detector.tokenize(text.lower().strip())
-    stopped_sent = []
+def fetchFromDb (query, collect, projection="", limitTo=0,db='yelp', pandas=False):
+    client = MongoClient()
+    queryAsDict = dict(itertools.izip_longest(*[iter(query)] * 2, fillvalue=""))
+    projectionAsDict = dict(itertools.izip_longest(*[iter(projection)] * 2, fillvalue=""))
+    database = client[db]
+    collection = database[collect]
+    start = timer()
+    if limitTo == 0:
+        documents = collection.find(queryAsDict, projectionAsDict)
+    else:
+        documents = collection.find(queryAsDict, projectionAsDict).limit(limitTo)
+    end = timer()
+    print "DB lazy access in " + str(end - start) + "sec"
+    if not pandas:
+        return documents
+    df = pd.DataFrame(documents)
+    return df
 
-    # remove stopwords
-    for sentence in sentences:
-        words = [word for word in sentence.split() if word not in stopwords.words('english')]
-        joint = " ".join(words)
-        stopped_sent.append(joint)
+if __name__ == '__main__':
+    query = ["stars", 5]
+    projection = ["text", 1]
+    documents = fetchFromDb(query, 'reviews', projection, 20)
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    bigram = []
+    for doc in documents:
+        text = doc.get('text')
+        # smartish word tokenizer, not too reliable
+        sentences = sent_detector.tokenize(text.lower().strip())
+        stopped_sent = []
 
-    # create bigrams
+        # remove stopwords
+        for sentence in sentences:
+            words = [word for word in sentence.split() if word not in stopwords.words('english')]
+            joint = " ".join(words)
+            stopped_sent.append(joint)
 
-    for sentence in stopped_sent:
-        bigram += nltk.bigrams(sentence.split())
+        # create bigrams
 
-fdist = nltk.FreqDist(bigram)
-for k, v in fdist.items():
-    print k,v
+        for sentence in stopped_sent:
+            bigram += nltk.bigrams(sentence.split())
+
+    fdist = nltk.FreqDist(bigram)
+    for k, v in fdist.items():
+        print k,v
+
+
+
+
