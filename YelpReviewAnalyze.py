0fe2cf7afc4865878bd7cd372a5c334b13bc1bdc
Pranabesh Sinha <prnbs@Pranabeshs-MacBook-Pro.local>
2015-08-21 17:40:57 -0700

    beginning multiprocessing

--- YelpReviewAnalyze.py
+++ YelpReviewAnalyze.py
@@ -8,37 +8,44 @@ import operator
 import re
 
 
-if __name__ == '__main__':
-    query = ["stars", 5]
-    projection = ["text", 1]
-    documents = PyM.fetchFromDb(query, 'reviews', projection, 3000)
-    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+def createNGram(doc):
+    text = doc.get('text')
+    # smartish word tokenizer, not too reliable
+    sentences = sent_detector.tokenize(text.lower().strip())
+    stopped_sent = []
     ngram = []
-    starttime = timer()
     pattern=re.compile("[^\w']")
-    for doc in documents:
-        text = doc.get('text')
-        # smartish word tokenizer, not too reliable
-        sentences = sent_detector.tokenize(text.lower().strip())
-        stopped_sent = []
-
+    # remove stopwords
+    for sentence in sentences:
+        # print sentence
+        # clear up punctuations
+        cleanedSentence = str(pattern.sub(' ', sentence))
         # remove stopwords
-        for sentence in sentences:
-            # print sentence
-            # clear up punctuations
-            cleanedSentence = str(pattern.sub(' ', sentence))
-            # remove stopwords
-            words = [word for word in cleanedSentence.split() if word not in stopwords.words('english')]
-            if len(words) > 0:
-                joint = " ".join(words)
-            # print joint
-            stopped_sent.append(joint)
-        # create bigrams
-        for sentence in stopped_sent:
-            ngram += nltk.bigrams(sentence.split())
+        words = [word for word in cleanedSentence.split() if word not in stopwords.words('english')]
+        if len(words) > 0:
+            joint = " ".join(words)
+        # print joint
+        stopped_sent.append(joint)
+    # create bigrams
+    for sentence in stopped_sent:
+        ngram += nltk.trigrams(sentence.split())
+    return ngram
+
+
+if __name__ == '__main__':
+    query = ["stars", 1]
+    projection = ["text", 1]
+    documents = PyM.fetchFromDb(query, 'reviews', projection, 24)
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    starttime = timer()
+    ngram = []
+    for doc in documents:
+        ngram.append(createNGram(doc))
+    flat_ngram_list = [item for sublist in ngram for item in sublist]
+
     endtime = timer()
 
-    fdist = nltk.FreqDist(ngram)
+    fdist = nltk.FreqDist(flat_ngram_list)
     sorted_x = sorted(fdist.items(), key=operator.itemgetter(1))
     for k in sorted_x:
         print " ".join(k[0]) + ":" + str(k[1])
